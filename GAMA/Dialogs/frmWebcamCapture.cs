552f1eab58780271a906218f0dc3eef91b8d1e26
Goo-DarTimejar <hosein.sk123@gmail.com>
2021-06-12 18:44:06 +0430

    add frmWebcamCapture from my older project (so it is not finished)

---
+++ GAMA/Dialogs/frmWebcamCapture.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using GAMA;
+
+namespace MyClass
+{
+    public partial class frmWebcamCapture : FrmMaster
+    {
+        public frmWebcamCapture()
+        {
+            InitializeComponent();
+        }
+
+        #region Fields
+
+        Image _image;
+        VideoCaptureDevice _webcam;
+        FilterInfoCollection _webcams;
+        private string _error;
+
+        #endregion
+
+
+        #region Properties
+
+        public Image WebcamCapture
+        {
+            get
+            {
+                return _image;
+            }
+            set
+            {
+                _image = value;
+                picWebcam.Image = value;
+            }
+        }
+
+        #endregion
+
+
+        #region Event Implication
+
+        private void FrmWebcamCapture_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddEllipse(picWebcam.DisplayRectangle);
+            picWebcam.Region = new Region(path);
+
+            _webcams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            cmbCameras.DataSource = _webcams;
+            cmbCameras.DisplayMember = "Name";
+            if (cmbCameras.Items.Count > 0)
+            cmbCameras.SelectedIndex = 0;
+
+            Image temp = Image.FromFile("NoImage.png");
+            picWebcam.Image = new Bitmap(temp);
+            temp.Dispose();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (cmbCameras.Items.Count == 0)
+            {
+                ShowError("دستگاهی برای ضبط ویدئو موجود نیست");
+                return;
+            }
+            if (_webcam == null || !_webcam.IsRunning)
+            {
+                picWebcam.Tag = "HadRun";
+                _webcam = new VideoCaptureDevice(((FilterInfo)cmbCameras.SelectedItem).MonikerString);
+                _webcam.NewFrame += Webcam_NewFrame;
+                _webcam.Start();
+            }
+            else
+            {
+                _webcam.Stop();
+            }
+        }
+
+        private void Webcam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap img = eventArgs.Frame;
+            img.RotateFlip(RotateFlipType.Rotate180FlipX);
+            picWebcam.Image = new Bitmap(img);
+            img.Dispose();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (timer1.Interval != 20)
+                timer1.Interval = 20;
+            _error = _error.Substring(0, _error.Length - 1);
+            lblError.Text = _error;
+            if (_error.Length == 0)
+                timer1.Stop();
+        }
+
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            if (picWebcam.Tag == null)
+            {
+                ShowError("لطفا از وبکم عکس بگیرید");
+                return;
+            }
+            WebcamCapture = picWebcam.Image;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        #endregion
+
+
+        #region Functions
+
+        private void ShowError(string error)
+        {
+            _error = error;
+            lblError.Text = _error;
+            lblError.Width = lblError.PreferredWidth;
+            lblError.Left = this.ClientSize.Width - lblError.Width;
+            timer1.Interval = 3000;
+            timer1.Start();
+        }
+
+        #endregion
+    }
+}
---
+++ GAMA/Dialogs/frmWebcamCapture.designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MyClass
+{
+    partial class frmWebcamCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.cmbCameras = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.picWebcam = new GAMA.PctrBSimple();
+            ((System.ComponentModel.ISupportInitialize)(this.picWebcam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.Location = new System.Drawing.Point(399, 206);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(134, 31);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "شروع دوربین";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // cmbCameras
+            // 
+            this.cmbCameras.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCameras.FormattingEnabled = true;
+            this.cmbCameras.Location = new System.Drawing.Point(376, 71);
+            this.cmbCameras.Name = "cmbCameras";
+            this.cmbCameras.Size = new System.Drawing.Size(175, 27);
+            this.cmbCameras.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(380, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "یک دوربین را انتخاب کنید";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmit.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.White;
+            this.btnSubmit.Location = new System.Drawing.Point(399, 243);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(134, 31);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "ثبت";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(399, 280);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(134, 31);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "انصراف";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(498, 321);
+            this.lblError.Name = "lblError";
+            this.lblError.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblError.Size = new System.Drawing.Size(0, 19);
+            this.lblError.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // picWebcam
+            // 
+            this.picWebcam.ColumnIndex = 0;
+            this.picWebcam.Location = new System.Drawing.Point(12, 49);
+            this.picWebcam.Name = "picWebcam";
+            this.picWebcam.PareName = null;
+            this.picWebcam.RowIndex = 0;
+            this.picWebcam.Size = new System.Drawing.Size(345, 319);
+            this.picWebcam.TabIndex = 8;
+            this.picWebcam.TabStop = false;
+            // 
+            // frmWebcamCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(564, 380);
+            this.Controls.Add(this.picWebcam);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCameras);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSubmit);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "frmWebcamCapture";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "وبکم";
+            this.Load += new System.EventHandler(this.FrmWebcamCapture_Load);
+            this.Controls.SetChildIndex(this.btnSubmit, 0);
+            this.Controls.SetChildIndex(this.btnCancel, 0);
+            this.Controls.SetChildIndex(this.btnStart, 0);
+            this.Controls.SetChildIndex(this.cmbCameras, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.lblError, 0);
+            this.Controls.SetChildIndex(this.picWebcam, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.picWebcam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox cmbCameras;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Timer timer1;
+        private GAMA.PctrBSimple picWebcam;
+    }
+}
